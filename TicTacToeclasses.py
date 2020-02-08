9d6a913e96d0f88a070fb46072068bbbc620e81d
acolbert1 <acolbert1@gmail.com>
2020-02-07 16:51:45 -0800

    added more functionality.

--- TicTacToeclasses.py
+++ TicTacToeclasses.py
@@ -1,3 +1,8 @@
+#current problems
+#The game is not taking the correct turns switching sides after the game is over
+#need to figure out how to check if its 3 Xs or 3 Os, the player currently assigned to X or O wins that round
+#add functionality to print how many times each player won during this set of games before the game is quit
+#combine piece information into a method
 import random
 import time
 import pprint
@@ -5,30 +10,78 @@ import pprint
 class Game(object):
     def __init__(self, piece):
         self.piece = piece
-        self.board =  {"0": "-", "1": "-", "2": "-", "3": "-", "4": "-", "5": "-", "6": "-", "7": "-", "8": "-", "9": "-"}
-        # self.board =  {0: "-", 1: "-", 2: "-", 3: "-", 4: "-", 5: "-", 6: "-", 7: "-", 8: "-", 9: "-"}
+        self.board =  {"1": "-", "2": "-", "3": "-", "4": "-", "5": "-", "6": "-", "7": "-", "8": "-", "9": "-"}
         self.playerOne = "1"
         self.playerTwo = "2"
         
     def playerMove(self):
-        if "-" not in self.board.values():
-                user_input = input("The game is a tie! Would you like to play again?")
-                user_input = user_input.lower()
-                if user_input in("y", "yes"):
-                    print("lets play again")
-                    self.board =  {"0": "-", "1": "-", "2": "-", "3": "-", "4": "-", "5": "-", "6": "-", "7": "-", "8": "-", "9": "-"}
-                    # self.playerMove()
-                else:
-                    print("Thanks for playing! Quitting the game.")
-                    exit()
-                
-            
-                # self.playerInput()
-                            
-
+        print(self.piece)
+        if self.board["1"] == "X" and self.board["2"] == "X" and self.board["3"] == "X":
+            print("Player 1 wins!")
+        if self.board["1"] == "O" and self.board["2"] == "O" and self.board["3"] == "O":
+            print("player 2 wins!")
+            self.gameOver()
+        if self.board["1"] == "X" and self.board["4"] == "X" and self.board["7"] == "X":
+            print("Player 1 wins!")
+            self.gameOver()
+        if self.board["1"] == "O" and self.board["4"] == "O" and self.board["7"] == "O":
+            print("Player 2 wins!")
+            self.gameOver()
+        if self.board["1"] == "X" and self.board["5"] == "X" and self.board["9"] == "X":
+            print("Player 1 wins!")
+            self.gameOver()
+        if self.board["2"] == "X" and self.board["5"] == "X" and self.board["8"] == "X":
+            print("Player 1 wins!")
+            self.gameOver()
+        if self.board["3"] == "X" and self.board["5"] == "X" and self.board["7"] == "X":
+            print("Player 1 wins!")
+            self.gameOver()
+        if self.board["3"] == "X" and self.board["6"] == "X" and self.board["9"] == "X":
+            print("Player 1 wins!")
+            self.gameOver()
+        if self.board["4"] == "X" and self.board["5"] == "X" and self.board["6"] == "X":
+            print("Player 1 wins!")
+            self.gameOver()
+        if self.board["7"] == "X" and self.board["8"] == "X" and self.board["9"] == "X":
+            print("Player 1 wins!")
+            self.gameOver()
         
+        self.tieGame()
+                                          
+    def gameOver(self):
+        user_input = input("The game is over! Player X wins! Would you like to play again or quit?")  
+        user_input = user_input.lower()
+        if user_input in ("y", "yes"):
+            print("Lets play again. Players switching sides.")
+            print(self.piece)
+            self.board =  {"1": "-", "2": "-", "3": "-", "4": "-", "5": "-", "6": "-", "7": "-", "8": "-", "9": "-"}
+            if self.piece == "X":
+                self.piece = "O"
+            if self.piece == "O":
+                self.piece = "X"
+            # self.playerMove()
+        else:
+            print("Thanks for playing! Quitting the game.")
+            exit()
+        
+    def tieGame(self):
+        if "-" not in self.board.values():
+            user_input = input("The game is a tie! Would you like to play again?")  
+            user_input = user_input.lower()
+            if user_input in ("y", "yes"):
+                print("Lets play again. Players switching sides.")
+                print(self.piece)
+                self.board =  {"1": "-", "2": "-", "3": "-", "4": "-", "5": "-", "6": "-", "7": "-", "8": "-", "9": "-"}
+                if self.piece == "X":
+                    self.piece = "O"
+                if self.piece == "O":
+                    self.piece = "X"
+                # self.playerMove()
+            else:
+                print("Thanks for playing! Quitting the game.")
+                exit()
 
-    def playerInput(self):
+    def playerInput(self): 
         position = self.setPosition(self.playerOne) #this sets the position for player 1
         print("Position = " + str(position)) # This prints the position that player 1 chooses
         if self.board.get(position) == "-":
@@ -48,18 +101,16 @@ class Game(object):
         position = self.setPosition(self.playerTwo) #this sets the position for player 2
         if self.piece == "X":
             playerTwoPiece = "O"
-        else:
+        if self.piece == "O":
             playerTwoPiece = "X"
+        # else:
+        #     playerTwoPiece = "X"
         
         if self.board.get(position) == "-":
             self.board[position] = playerTwoPiece
             print(self.board)
             self.playerMove()
 
-        # if self.board[position] == "-":
-        #     self.board[position] = playerTwoPiece
-        #     print(self.board)
-        #     self.playerMove()
         else:
             print("That position already has a piece. Please choose another.")
             print(self.board)
@@ -85,13 +136,7 @@ class Game(object):
         #     return positionInput
         # while positionInput != int:
         #     print("This is not a number")
-        
-        
 
-
-#if the input is a number, check the dictionary key and assign the players piece to that vaue
-#if the input is a number isnt a key in teh dictionary, tell the user that number is not a position on the board
-#if the user inputs a character that isnt a number, tell them to enter a number between 0-9. 
 
         # self.board.keys()
         # while True:
@@ -110,21 +155,9 @@ class Game(object):
 
     
 
-game1 = Game("O")
+game1 = Game("X")
 game1.playerInput()
 
- # playerChoice = input("Starting the first round. Who will be Player 1? ")
-        # if self.player == 1:
-        #     self.playerTwo = 2
-        # else:
-        #     self.playerTwo = 1
-        # print(self.playerTwo)
-
-
-#add functionality to have the user reinput a command if the input is not X or O, also y or yes
-#add functionlity that allows the players to switch sides
-#add pattern functionality to determine winner
-#add functionality to print how many times each player won during this set of games before the game is quit
 
 
 # def setPosition(self, player):
@@ -138,5 +171,3 @@ game1.playerInput()
 #             print("This isnt a number")
 #         return self.setPosition
 
-
-#if the input is a number but is not one of the keys, print somethign, and then check again
